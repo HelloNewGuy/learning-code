fda6222a11001f33cd0645bc500210edd4802796
Juan Cruz <j.pablo.cruz.p@gmail.com>
2025-07-03 20:28:34 -0400

    Password prompt

---
+++ PassWithWhile.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PassWithWhile {
+
+    private static final String CORRECT_PASSWORD = "LearningJava!";
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String password;
+
+        System.out.print("Enter password: ");
+        password = scanner.nextLine();
+
+        while (!password.equals(CORRECT_PASSWORD)) {
+            System.out.println("Incorrect password. Try again.");
+
+            System.out.print("Enter password: ");
+            password = scanner.nextLine();
+        }
+        System.out.print("Access Granted!");
+        scanner.close();
+    }
+}
