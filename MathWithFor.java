0dd3b5af33c2405168db6896b071903dfdd06305
Juan Cruz <j.pablo.cruz.p@gmail.com>
2025-07-03 20:06:59 -0400

    Multiplication Table

---
+++ MathWithFor.java
@@ -0,0 +1,13 @@
+public class MathWithFor {
+
+    public static void main(String[] args){
+        final int FACTOR = 7;
+        final int MAX_MULTIPLIER = 12;
+
+        for (int i = 1; i <= MAX_MULTIPLIER; i++){
+            int product = FACTOR * i;
+            System.out.printf("%d x %d = %d%n", FACTOR, i, product);
+        }
+    }
+}
+//We can always replace the factor and get the multiplication table for all numbers
